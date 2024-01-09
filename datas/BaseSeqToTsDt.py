ac420b64737e4e02f85e4a3f8be4271bb9abe3ae
zorrro_gridi <zorro_li@icloud.com>
2024-01-09 14:02:18 +0800

    add data application proj

--- datas/BaseSeqToTsDt.py
+++ datas/BaseSeqToTsDt.py
@@ -13,15 +13,9 @@ import logging
 
 
 
-
 class BaseSeqToTsDt(AbstractDatasetFactory):
-    def __init__(self, input_features=None, **kwargs):
-        '''
-        # input_features: 时间序列的外部变量列表
-        '''
+    def __init__(self, **kwargs):
         super(BaseSeqToTsDt, self).__init__(**kwargs)
-        # input_features 作为外部变量
-        self.input_features = input_features
         # 时间序列的特征是窗口长度
         if isinstance(self.features, list):
             self.features = len(self.features)
@@ -34,31 +28,16 @@ class BaseSeqToTsDt(AbstractDatasetFactory):
     def feature_engineering(self, vars_datas):
         '''
         # vars_datas: 数组对象
-        # preprocess_func: 对原始数据进行特征呢工程之前，预处理的函数。可以将定义的预处理函数封装成偏函数
         '''
         # 时间序列的特征定义
         window = self.features + self.target
-        if self.preprocess_func is not None and self.input_features is not None:
-            prep_func = partial(self.preprocess_func, input_features=self.input_features)
-            vars_datas = prep_func(vars_datas)
-
-        elif self.preprocess_func is not None and self.input_features is None:
-            vars_datas = self.preprocess_func(vars_datas)
-
-        logging.warning(f'var datas preview: {vars_datas[0]}')
-
-        model_types_options = ['nn', 'boost']
-        if self.model_type not in model_types_options:
-            logging.warning(f'Supported model types options: {model_types_options}')
-            raise Exception
-
         vars_datas_copy = vars_datas.copy()
+        # 时间序列处理的核心
         vars_datasets = [vars_datas_copy[i:i+window] for i in range(len(vars_datas_copy))]
 
         # 将不满足长度的数据全部删除，数据信息没有影响
         vars_datasets = [v for v in vars_datasets if len(v) == window]
         vars_datasets = np.array(vars_datasets)
-
         return vars_datasets
 
 
---
+++ datas/application/IndexMarkupTsDt.py
@@ -0,0 +1,71 @@
+from mlops.datas.BaseSeqToTsDt import BaseSeqToTsDt
+from functools import partial
+import torch
+import numpy as np
+
+from torch.utils.data import (
+    Dataset,
+    DataLoader,
+    random_split,
+    )
+
+
+import logging
+
+
+
+class BaseIndexMarkupTsDt(BaseSeqToTsDt):
+    def __init__(self, input_features=None, **kwargs):
+        '''
+        # input_features: 时间序列的外部变量列表
+        '''
+        super().__init__(**kwargs)
+
+        # input_features 作为外部变量
+        self.input_features = input_features
+        # 时间序列的特征是窗口长度
+        if isinstance(self.features, list):
+            self.features = len(self.features)
+
+
+    def feature_engineering(self, vars_datas):
+        '''
+        # vars_datas: 数组对象
+        # preprocess_func: 对原始数据进行特征呢工程之前，预处理的函数。可以将定义的预处理函数封装成偏函数
+        '''
+        if self.preprocess_func is not None and self.input_features is not None:
+            prep_func = partial(self.preprocess_func, input_features=self.input_features)
+            vars_datas = prep_func(vars_datas)
+
+        elif self.preprocess_func is not None and self.input_features is None:
+            vars_datas = self.preprocess_func(vars_datas)
+
+        logging.warning(f'var datas preview: {vars_datas[0]}')
+
+        vars_datasets = super().feature_engineering(vars_datas)
+        return vars_datasets
+
+
+
+class IndexMarkupTsDt_NN(BaseIndexMarkupTsDt):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+    def feature_engineering(self, vars_datas):
+        vars_datasets = super().feature_engineering(vars_datas)
+
+        window = self.features + self.target
+        # 神经网络模型需要 batch_size 三维数据
+        vars_datasets = vars_datasets.reshape(len(vars_datasets), window, -1)
+        vars_datasets = vars_datasets.astype(np.float32)
+        vars_datasets = torch.from_numpy(vars_datasets)
+        return vars_datasets
+
+
+    def data_split(self, raw_dataset, test_size=0.2, random_state=42):
+        generator = torch.Generator().manual_seed(random_state)
+        train_dataset, test_dataset = random_split(
+            raw_dataset, lengths=[1-test_size, test_size], generator=generator)
+
+        return train_dataset, test_dataset
