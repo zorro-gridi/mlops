a5e5b432b59d150af2b77dca7209f23c13a87ced
zorro-gridi <{ID}+{username}@users.noreply.github.com>
2024-01-18 16:01:03 +0800

    add catboost tsak

--- ml_ops/cat_ops.py
+++ ml_ops/cat_ops.py
@@ -1,2 +1,28 @@
-# undefined
-test = 1
+from mlops.ml_ops.base import AbstractMLOps
+from mlops.tasks.cat import CatboostTask
+
+import mlflow
+from mlflow.models import infer_signature
+from mlops.utils import mlflow_utils
+from mlflow.client import MlflowClient
+
+
+import logging
+import shutil
+from copy import copy
+
+
+class XgboostOps(AbstractMLOps):
+    def __init__(self, **kwargs):
+        super(XgboostOps, self).__init__(**kwargs)
+
+
+    def find_best_data_args(self, params_space):
+        best_checkpoint = self.model_task.tune_job(
+            params_space,
+            train_data=self.train_data,
+            test_data=self.test_data,
+            )
+
+        self.best_model_args = copy(self.model_task.model_init_params)
+        return best_checkpoint
--- tasks/cat.py
+++ tasks/cat.py
@@ -1,5 +1,5 @@
-# from mlops.tasks.base import AbstractModelFactory
-from base import AbstractModelFactory
+from mlops.tasks.base import AbstractModelFactory
+# from base import AbstractModelFactory
 
 from catboost import (
     CatBoost,
@@ -19,43 +19,52 @@ from copy import copy
 class CatboostTask(AbstractModelFactory):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.model_init_params['loss_function'] = self.model_eval_metric
+        self.model_init_params['custom_metric'] = self.custom_loss_func
+
+        baseline_params = {
+            'random_seed': 42,
+            'logging_level': 'Silent',
+            # 返回 eval_set 上的最优模型
+            'use_best_model': True,
+            }
+        self.model_init_params.update(baseline_params)
+
+        earlystop_params = {
+            'od_type': 'Iter',
+            'od_wait': 40
+            }
+        self.model_init_params.update(earlystop_params)
 
 
-    # def train_job(self, config, train_datat, test_data):
-    #     trial_model = CatBoost(
-    #         l2_leaf_reg=int(config['l2_leaf_reg']),
-    #         learning_rate=config['learning_rate'],
-    #         loss_function=self.model_eval_metric,
-    #         custom_metric=self.custom_loss_func,
-    #         **self.model_train_params,
-    #         )
-
-    #     cv_pool = Pool(*train_datat)
-    #     cv_data = cv(cv_pool, trial_model.get_params(), logging_level='Silent')
-    #     # cv 返回交叉验证的评估指标
-    #     best_loss = np.min(cv_data[f'test-{self.model_eval_metric}-mean'])
-    #     return best_loss
-
-
-    def train_job(self, config, train_datat, test_data):
-        config['loss_function'] = self.model_eval_metric
-        config['custom_metric'] = self.custom_loss_func
-
+    def train_job(self, config, train_data, test_data):
         model_params = copy(config)
         model_params.update(self.model_init_params)
-        model_params.update(self.model_train_params)
         # CatBoost 接受字典类型参数
         trial_model = CatBoost(model_params)
 
-        train_pool = Pool(*train_datat)
+        train_pool = Pool(*train_data)
         test_pool = Pool(*test_data)
-        trial_model.fit(train_pool, eval_set=test_pool)
-        best_iteration = trial_model.get_best_iteration()
+
+        x_train, y_train = train_data
+        x_test, y_test = test_data
+        X = np.concatenate([x_train, x_test], axis=0)
+        y = np.concatenate([y_train, y_test], axis=0)
+        cv_pool = Pool(X, y)
+
+        # cat_features, ... 都可以写入 self.model_train_params
+        trial_model.fit(train_pool, eval_set=test_pool, **self.model_train_params)
+        # best_iteration = trial_model.get_best_iteration()
         best_score = trial_model.get_best_score()
         test_loss = best_score["validation"][self.model_eval_metric]
+        cv_data = cv(cv_pool, trial_model.get_params(), logging_level='Silent')
 
-        logging.warning(f'CatBoost model test loss: {test_loss}')
-        return test_loss
+        # cv 返回交叉验证的评估指标
+        cv_loss = np.min(cv_data[f'test-{self.model_eval_metric}-mean'])
+        if self.optimize_mode == 'max':
+            cv_loss = 1 / cv_loss
+        logging.warning(f'CatBoost model test loss: {test_loss:,.6f}, cv loss: {cv_loss:,.6f}')
+        return cv_loss
 
 
     def tune_job(self, params_space, train_data, test_data):
@@ -70,7 +79,26 @@ class CatboostTask(AbstractModelFactory):
             trials=trials,
             rstate=np.random.default_rng(random.seed(42))
             )
-        return best_params
+        logging.warning(f'CatBoost best tune model params: {best_params}')
+
+        self.model_init_params.update(best_params)
+        best_model = CatBoost(self.model_init_params)
+
+        train_pool = Pool(*train_data)
+        test_pool = Pool(*test_data)
+
+        best_model.fit(train_pool, eval_set=test_pool, **self.model_train_params)
+        test_loss = self.test_job(best_model, test_pool)
+
+        return {
+            'best_model': best_model,
+            self.model_eval_metric: test_loss,
+            }
+
+
+    def test_job(self, model: CatBoost, test_pool):
+        test_loss = model.eval_metrics(test_pool, [self.model_eval_metric])
+        return test_loss
 
 
 
@@ -83,11 +111,11 @@ if __name__ == '__main__':
 
     cat_task = CatboostTask(model_eval_metric='Logloss')
 
-    config = {
-        'l2_leaf_reg': 3.0,
-        'learning_rate': 0.2,
+    params_space = {
+        'l2_leaf_reg': hyperopt.hp.qloguniform('l2_leaf_reg', 0, 2, 1),
+        'learning_rate': hyperopt.hp.uniform('learning_rate', 1e-3, 5e-1),
         }
 
     train_datas = (train_data, train_labels)
     test_datas = (test_data, test_labels)
-    cat_task.train_job(config, train_datas, test_datas)
+    cat_task.tune_job(params_space, train_datas, test_datas)
