a58491d8444b87e35c0d4550bde9b03a64a2596d
zorro-gridi <{ID}+{username}@users.noreply.github.com>
2024-12-13 11:54:59 +0800

    update

--- src/mlops/algo_box/seq_utils.py
+++ src/mlops/algo_box/seq_utils.py
@@ -724,8 +724,10 @@ if __name__ == '__main__':
     min_chg = 3 / 100
     peak_trough_detecter = Peak_and_Trough_Detecter(min_chg)
 
-    peak_detecter_result = peak_trough_detecter.fit(fund_values, point_type='peak', base_point='trough', end_point='phase')
-    trough_detecter_result = peak_trough_detecter.fit(fund_values, point_type='trough', base_point='peak', end_point='phase')
+    peak_detecter_result = peak_trough_detecter.fit(
+        fund_values, point_type='peak', base_point='trough', end_point='phase')
+    trough_detecter_result = peak_trough_detecter.fit(
+        fund_values, point_type='trough', base_point='peak', end_point='phase')
 
     # %%
     peak_trough_detecter.fit(fund_values, base_point='max_peak', end_point='current')
