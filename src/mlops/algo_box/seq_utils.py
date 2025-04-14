a718c348f03a55b4589cccc776248626162bd701
zorro-gridi <{ID}+{username}@users.noreply.github.com>
2025-04-14 21:47:08 +0800

    update

--- src/mlops/algo_box/seq_utils.py
+++ src/mlops/algo_box/seq_utils.py
@@ -989,7 +989,7 @@ def peak_trough_detect_table(fundcode, min_chg, drange=720, make_plot=False):
     current_stage = peak_trough_detecter.get_current_stage()
     logging.warning(f'基金 {fundcode} 当前处于: 【{current_stage}】阶段!')
 
-    # NOTE: 对于债券基金来说，因为走势整体基本是向上的，因此，阶段回撤修复收益的占历史阶段收益的百分位数可能经常出现 0% 的情况
+    # NOTE: 对于债券基金来说，因为走势整体基本是向上的，因此，阶段回撤修复收益占历史阶段收益的百分位数可能经常出现 0% 的情况
     # 因此，阶段收益百分位数指标可能不适合分析债券基金
     if current_stage == 'peak':
         logging.warning(f'当前【回撤修复】的累计收益: {lastTough2curr_yield:0.4f}，收益百分位数: {trough2curr_percentile * 100}%; 后续潜在[回撤范围]: {potential_trough_range}')
